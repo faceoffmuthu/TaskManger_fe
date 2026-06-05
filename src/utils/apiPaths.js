a1f05059a4413a77ae30edb15dff727b0fa0e079
muthu <faceoffmuthu@gmail.com>
2026-06-05 15:26:50 +0530

    backend url add

--- src/utils/apiPaths.js
+++ src/utils/apiPaths.js
@@ -1,8 +1,8 @@
-export const BASE_URL = "http://localhost:8000";
+export const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:8000";
 
 // utils/apiPaths.js
 export const AUTH_PATHS = {
-  AUTH :{
+  AUTH: {
     REGISTER: "api/auth/register", //Register a new user (Admin or Member)
     LOGIN: "api/auth/login", //Authenticatr user & return JWT token
     GET_PROFILE: "api/auth/profile", //Get logged in user details
@@ -14,7 +14,7 @@ export const AUTH_PATHS = {
     CREATE_USER: "api/users", //Create a new user (Admin only)
     UPDATE_USER: (userId) => `api/users/${userId}`, //Update user (Admin only)
     DELETE_USER: (userId) => `api/users/${userId}`, //Delete user (Admin only)
-    
+
   },
   TASKS: {
     GET_DASHBOARD_DATA: "api/tasks/dashboard-data", //Get dashboard data (Admin only)
@@ -26,13 +26,13 @@ export const AUTH_PATHS = {
     DELETE_TASK: (taskId) => `api/tasks/${taskId}`, //Delete task (Admin only)
 
     UPDATE_TASK_STATUS: (taskId) => `api/tasks/${taskId}/status`, //Update task status (Admin only)
-    UPDATE_TODO_CHECKLIST:(taskId)=>`api/tasks/${taskId}/todo`, //Update todo checklist (Admin only)
+    UPDATE_TODO_CHECKLIST: (taskId) => `api/tasks/${taskId}/todo`, //Update todo checklist (Admin only)
   },
-  REPORTS:{
-    EXPORT_TASKS:"api/reports/export/tasks", //Download tasks as Excel
-    EXPORT_USERS:"api/reports/export/users", //Download user-task report
+  REPORTS: {
+    EXPORT_TASKS: "api/reports/export/tasks", //Download tasks as Excel
+    EXPORT_USERS: "api/reports/export/users", //Download user-task report
   },
-  IMAGE:{
-    UPLOAD_IMAGE:"api/auth/upload-image", //Upload user profile image
+  IMAGE: {
+    UPLOAD_IMAGE: "api/auth/upload-image", //Upload user profile image
   }
 }
